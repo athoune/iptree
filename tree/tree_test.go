6501be0c463153f315549dc3dac3730748613e51
Mathieu Lecarme <mathieu@garambrogne.net>
2022-04-29 11:02:27 +0200

    Less bench.

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -1,9 +1,7 @@
 package tree
 
 import (
-	"compress/gzip"
 	"net"
-	"os"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
@@ -28,56 +26,5 @@ func BenchmarkContains(b *testing.B) {
 	a := net.ParseIP("192.168.1.42")
 	for i := 0; i < b.N; i++ {
 		nm.Contains(a)
-	}
-}
-
-func BenchmarkTree(b *testing.B) {
-	f, err := os.Open("../ip2asn-v4.tsv.gz")
-	assert.NoError(b, err)
-	r, err := gzip.NewReader(f)
-	assert.NoError(b, err)
-	tree := NewTrunk(2)
-	err = tree.FeedWithTSV(r)
-	assert.NoError(b, err)
-	freeS, err := net.LookupHost("free.fr")
-	assert.NoError(b, err)
-	var free net.IP
-	for _, f := range freeS {
-		i := net.ParseIP(f)
-		if i.To4() != nil {
-			free = i
-			break
-		}
-	}
-	assert.NotNil(b, free)
-	for i := 0; i < b.N; i++ {
-		_, ok := tree.Get(free)
-		assert.True(b, ok)
-	}
-}
-
-func BenchmarkCachedTree(b *testing.B) {
-	f, err := os.Open("../ip2asn-v4.tsv.gz")
-	assert.NoError(b, err)
-	r, err := gzip.NewReader(f)
-	assert.NoError(b, err)
-	tree, err := NewCachedTrunk(256, 2)
-	assert.NoError(b, err)
-	err = tree.FeedWithTSV(r)
-	assert.NoError(b, err)
-	freeS, err := net.LookupHost("google.fr")
-	assert.NoError(b, err)
-	var free net.IP
-	for _, f := range freeS {
-		i := net.ParseIP(f)
-		if i.To4() != nil {
-			free = i
-			break
-		}
-	}
-	assert.NotNil(b, free)
-	for i := 0; i < b.N; i++ {
-		_, ok := tree.Get(free)
-		assert.True(b, ok)
 	}
 }
